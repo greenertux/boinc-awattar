e2529ef92b20f94596fa61f8b790589dba345a56
Tristan <13280348+greenertux@users.noreply.github.com>
2021-05-28 20:50:01 +0200

    Add check for current data
    On systems that don't run 24/7 we cannot assume current data will be downloaded at midnight.
    This improved version will check whether the data is current and if not update directly from the API.

--- controller.sh
+++ controller.sh
@@ -5,19 +5,35 @@ start_price=2 # start below this price
 file=/path/to/current.yaml
 link=https://api.awattar.de/v1/marketdata/current.yaml
 
-if test -f "$file"; then
-  echo "$file exists"
-else
-  wget $link -O $file
+function download_data { 
+  wget $link -O $file; 
   if test -f "$file"; then
     echo "$file downloaded"
   else
     echo "could not get prices"
     exit 1
   fi
+}
+function get_current_price { current_price=$(sed -n $((2*$(date +%-H)+39)){p} $file | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | tail -n1); }
+function get_current_day { current_day=$(sed -n 3{p} $file | grep -Eo '[0-9]+'); }
+
+# test if data file exists
+if test -f "$file"; then
+  echo "$file exists"
+  # test if data is current
+  get_current_day
+  if [ "$current_day" = "$(date +%-d)" ]; then
+    echo "Awattar data is up to date"
+  else 
+    echo "Awattar data is outdated, fetching new data"
+    rm $file
+    download_data
+  fi
+else # data file does not exist
+  download_data
 fi
 
-current_price=$(sed -n $((2*$(date +%-H)+39)){p} $file | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | tail -n1)
+get_current_price
 echo "current price is" $current_price
 if (( $(echo "$stop_price < $start_price" |bc -l) )); then
   echo "stop price cannot be lower than start price"
